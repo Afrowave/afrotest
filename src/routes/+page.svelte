d5084402dc2973de17bdf7e2becd0d3d7aabc407
Jimmy Gitonga <jimmy@afroshok.com>
2023-11-04 15:12:38 +0300

    second commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Welcome to SvelteKit built with Bun</h1>
+
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation and start developing your app.</p>
+<p>If you want to want to start a Sveltekit project with the <em>bun</em> runtime, visit <a href="https://bun.sh/guides/ecosystem/sveltekit">Build an app with SvelteKit and Bun</a>.</p>
+<p>If you will deploy your Sveltekit app or website on Cloudflare, visit <a href="https://developers.cloudflare.com/pages/framework-guides/deploy-a-svelte-site/">Deploy a Svelte site</a> page. <br>
+  It is important to build and deploy the initial commit to Cloudflare so that future deployments move forward without a hitch.
+</p>
+
